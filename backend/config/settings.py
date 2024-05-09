0d1b7e4a739068bd3611304535abd6594a3b8217
Nicolò Poggi <pogginicolo@proton.me>
2024-05-09 17:06:54 +0200

    :fire: Removed session authentication

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -141,7 +141,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
