c4f18518b94c6300f3f3ef4e267b04b82319b8ff
Nicolò Poggi <pogginicolo@proton.me>
2024-05-09 09:35:24 +0200

    :sparkles: Added coverage conf

---
+++ backend/test_coverage.sh
@@ -0,0 +1,6 @@
+/home/moltack/two-guys-one-hike/software/roots/.coveragerc#!/bin/sh
+
+set -e  # Configure shell so that if one command fails, it exits
+coverage erase
+coverage run manage.py test apps/account apps/core apps/post
+coverage report
